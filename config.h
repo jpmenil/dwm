aa694be795f9b06d6c5742282d92f9447b71d137
Jean-Philippe Menil <jpmenil@gmail.com>
2021-03-17 19:02:20 +0100

    fixup! fix rules
    Signed-off-by: Jean-Philippe Menil <jpmenil@gmail.com>

--- config.h
+++ config.h
@@ -44,7 +44,7 @@ static const Rule rules[] = {
 	 */
         /* class      instance    title       tags mask     isfloating   monitor */
         { "Chromium", NULL,       NULL,       1 << 6,       0,           -1 },
-	{ mutt,       NULL,       NULL,       1 << 7,       0,           -1 },
+	{ "mutt",     NULL,       NULL,       1 << 7,       0,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
